f2d9ef518a4a8badad226a97bfe85b669bfc3fd5
MarvinMok <marvinjmok@gmail.com>
2024-05-31 15:25:19 -0700

    connected servo to potentiometer

--- pico/servotest.py
+++ pico/servotest.py
@@ -1,10 +1,16 @@
 from servo import Servo
+from machine import Pin, ADC
 import time
 
 sg90_servo = Servo(pin=16)  #To be changed according to the pin used
-
+adc = ADC(Pin(26))
+deg = 0
+sg90_servo.move(0)
+time.sleep(0.5)
 while True:
-    sg90_servo.move(0)  # turns the servo to 0°.
-    time.sleep(1)
-    sg90_servo.move(180)  # turns the servo to 90°.
-    time.sleep(1)
+    temp = adc.read_u16() * 180 // 65535
+    #print(temp, adc.read_u16() * 180)
+    if temp != deg:        
+        deg = temp
+        sg90_servo.move(deg)  # turns the servo to 0°.
+    time.sleep(0.1)
